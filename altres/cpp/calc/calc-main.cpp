f8f78c2c23c52e38d54cb6a3d566a183dea6f4c6
pauSSe <pauseawesome@protonmail.com>
2020-12-11 16:50:52 +0100

    Millorada la calculadora en C++

--- altres/cpp/calc/calc-main.cpp
+++ altres/cpp/calc/calc-main.cpp
@@ -1,27 +1,38 @@
 #include <iostream>
 
 int main() {
-  /* code */
+  // Variables
   float n1, n2, res;
   char op;
 
-  std::cout << "Number" << '\n';
-  std::cin >> n1;
-  std::cout << "\nOperator" << '\n';
+  //Lectura i escriptura
+  std::cout << "Numbers (2)\n";
+  std::cin >> n1 >> n2;
+  std::cout << "Operació" << '\n';
   std::cin >> op;
-  std::cout << "\nNumber" << '\n';
-  std::cin >> n2;
-  std::cout << "Operation" << '\n' << n1 << op << n2 << "\n";
 
+  //El switch de la calculadora
+  //Determina quina operació es farà segons l'operador (op)
   switch (op) {
     case '+':
       std::cout << n1 << "+" << n2 << '=' << n1 + n2 << '\n';
+      res = n1 + n2;
+      break;
     case '-':
-      std::cout << n1 << "-" << n2 << '=' << n1 - n2<< '\n';
+      std::cout << n1 << "-" << n2 << '=' << n1 - n2 << '\n';
+      res = n1 - n2;
+      break;
     case '*':
       std::cout << n1 << "*" << n2 << '=' << n1 * n2 << '\n';
+      res = n1 * n2;
+      break;
     case '/':
-      std::cout << n1 << "/" << n2 << '=' << n1 / n2<< '\n';
+      std::cout << n1 << "/" << n2 << '=' << n1 / n2 << '\n';
+      res = n1 / n2;
+      break;
+    default:
+      std::cout << "Bad Operator: " << op << " not found!\n";
+      break;
   }
 
   return 0;
