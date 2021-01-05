4d9765d0baa31feadbe4e4ef068841bea7c53dae
pswsm <pauseawesome@protonmail.com>
2021-01-05 03:07:06 +0100

    Basic nim, i els seus binaris. La calculadora no es una calculadora tho.

---
+++ nim/calc/calc.nim
@@ -0,0 +1,10 @@
+# Bevinguts a asquest detector de OSs fet en Nim
+
+when system.hostOS == "windows":
+  echo "running Windows, really?"
+elif system.hostOS == "linux":
+  echo "Hello, fellow linux user"
+elif system.hostOS == "macosx":
+  echo "Wow, a Mac user"
+else:
+  echo "What you running, mate?"
