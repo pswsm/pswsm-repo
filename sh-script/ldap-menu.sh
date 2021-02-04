4701e1f8677b4aae4ee9737d44f0c29d2bdf6355
pauSSe <pauseawesome@protonmail.com>
2021-02-04 20:05:56 +0100

    El creador de grups funciona, crec.

--- sh-script/ldap-menu.sh
+++ sh-script/ldap-menu.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-#_base="$(dirname $_script)"
+_script="$(readlink -f ${BASH_SOURCE[0]})"
+_base="$(dirname $_script)"
 
 while :
 do
+	clear
+
 	echo -e "\nLDAP: Menu d'usuari\nSelecciona l'acció desitjada\n\t(1) Crear nova UO\n\t(2) Crear un nou grup\n\t(3) Crear un nou usuari\n\t(4) Carregar a LDAP\n\t(5) Sortir"
 	read -p ":	" num
 
@@ -18,14 +21,17 @@ do
 
 		;;
 		2)
-                        clear  
+			# Crear Grups
+                        clear
+			file="$_base/groups"
+			#DEBUG:
+			#echo -e "\n"$file"\n"
                         echo -e "Vols fer-ho amb manualment (1) o amb l'assistent (2)?"
-                        read ed   
+                        read ed
                         case $ed in
                                 1)
                                         echo -e "És guradarà a groups.ldif\t"
-					file="groups.ldif"
-                                        $EDITOR $file
+                                        $EDITOR $file.ldif
                                 ;;
                                 2)
                                         echo -e "\nCreador de Grups\n"
@@ -33,12 +39,17 @@ do
                                         echo -e "\nNom de domini (ou=X,dc=Y,dc=tld):\t"
                                         read ub
 
-					if [ -f "$file" ]; then
-						gid=$(grep gid $file | cut -d " " -f2 | sort -d | tail -n 1)
+
+					if [ -f "$file.ldif" ]; then
+						gid=$(grep gid $file.ldif | cut -d " " -f2 | sort -d | tail -n 1)
 						((gid++))
 					else
-						gid=$(ldapsearch -x -LLL -b dc=edt,dc=org "(objectClass=posixGroup)" | grep gid | sort -d | cut -d " " -f2 | tail -n 1)
-						((gid++))
+						gidDB=$(ldapsearch -x -LLL -b dc=edt,dc=org "(objectClass=posixGroup)" | grep gid | sort -d | cut -d " " -f2 | tail -n 1)
+						if [ -z "$gidDB" ]; then
+							gid=500
+						else
+							gid=$((gidDB++))
+						fi
 					fi
 					
 					echo -e "El gid serà "$gid"."
@@ -46,22 +57,19 @@ do
                                         echo -e "\nNom del grup (cn=Nom):\t"
                                         read cn
                                         
-                                        echo -e "És guardarà a groups.ldif\t"
-					file="groups.ldif"
-                                                
-                                        if [ -f "$file" ]; then
+                                        if [ -f "$file.ldif" ]; then
                                                 echo -e "\nEl fitxer existeix, vols sobreescriure'l? [Y/n]\t"
                                                 read yn
                                                 case $yn in
                                                         y | Y)
-                                                                echo -e "dn:\t"$cn","$ub"\ngidNumber: "$gid"\ncn: "$cn"\nobjectClass: posixGroup\nobjectClass: top\n" > $file
+                                                                echo -e "dn:\t"$cn","$ub"\ngidNumber: "$gid"\ncn: "$cn"\nobjectClass: posixGroup\nobjectClass: top\n" > $file.ldif
                                                         ;;
                                                         n | N)
 								echo -e "El contingut serà afegit al final del fitxer.\n"
-								echo -e "dn:\t"$cn","$ub"\ngidNumber: "$gid"\ncn: "$cn"\nobjectClass: posixGroup\nobjectClass: top\n" >> $file
+								echo -e "dn:\t"$cn","$ub"\ngidNumber: "$gid"\ncn: "$cn"\nobjectClass: posixGroup\nobjectClass: top\n" >> $file.ldif
                                                 esac
                                         else
-                                                echo -e "dn: "$cn","$ub"\ncn: "$cn"\nobjectClass: posixGroup\nobjectClass: top\n" > $file.ldif
+                                                echo -e "dn: "$cn","$ub"\ngidNumber: "$gid"\ncn: "$cn"\nobjectClass: posixGroup\nobjectClass: top\n" > $file.ldif
 
                                         fi
                                         # $_base/assistent.sh
@@ -71,6 +79,7 @@ do
 
 		;;
 		1)
+			#Crear UOs
 			clear
 			echo -e "Vols fer-ho amb manualment (1) o amb l'assistent (2)?"
 			read ed
