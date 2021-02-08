e4cfc4e6888170c2ab7a19f7ab022eed2b9ca252
pswsm <pauseawesome@protonmail.com>
2021-02-08 23:59:44 +0100

    No rulava el creador de grups, he afegit el creador d'usuaris.

--- sh-script/ldap-menu.sh
+++ sh-script/ldap-menu.sh
@@ -66,7 +66,60 @@ do
 		;;
 		3)
 			# Crear Usuaris
+			clear
+			#DEBUG:
+			#echo -e "\n"$file"\n"
+			echo -e "Vols fer-ho amb manualment (1) o amb l'assistent (2)?"
+			read ed
+			case $ed in
+				1)
+					echo -e "És guradarà a users.ldif\t"
+					$EDITOR $fUsers
+					;;
+				2)
+					echo -e "\nCreador de Grups\n"
+					echo -e "\nNom de domini (ou=X,dc=Y,dc=tld):\t"
+					read ub
 
+					# Treure el gidNumber
+					if [ -f $fUsers ]; then
+						gidFile=$(grep gid $fUsers | cut -d " " -f2 | sort -d | tail -n 1)
+						((gidFile++))
+						gidDB=$(ldapsearch -x -LLL -b dc=edt,dc=org "(objectClass=posixGroup)" | grep gid | sort -d | cut -d " " -f2 | tail -n 1)
+						((gidDB++))
+						if [ $gidFile > $gidDB ]; then
+							gid=$gidFile
+						else
+							gid=$gidDB
+						fi
+					else
+						gid=$(ldapsearch -x -LLL -b dc=edt,dc=org "(objectClass=posixGroup)" | grep gid | sort -d | cut -d " " -f2 | tail -n 1)
+					fi
+
+					echo -e "El gid serà "$gid"."
+					echo -e "\nNom del grup (cn=Nom):\t"
+					read cn
+					if [ -f "$fUsers" ]; then
+						echo -e "\nEl fitxer existeix, vols sobreescriure'l? [Y/n]\t"
+						read yn
+						case $yn in
+							y | Y)
+								echo -e "dn:\t"$cn","$ub"\ngidNumber: "$gid"\ncn: "$cn"\nobjectClass: posixGroup\nobjectClass: top\n" > $fUsers
+								;;
+							n | N)
+								echo -e "El contingut serà afegit al final del fitxer.\n"
+								echo -e "dn:\t"$cn","$ub"\ngidNumber: "$gid"\ncn: "$cn"\nobjectClass: posixGroup\nobjectClass: top\n" >> $fUsers
+								;;
+								*)
+								echo -e "dn:\t"$cn","$ub"\ngidNumber: "$gid"\ncn: "$cn"\nobjectClass: posixGroup\nobjectClass: top\n" > $fUsers
+						esac
+					else
+						echo -e "dn: "$cn","$ub"\ngidNumber: "$gid"\ncn: "$cn"\nobjectClass: posixGroup\nobjectClass: top\n" > $fUsers
+					fi
+					# $_base/assistent.sh
+					# El fitxer assistent conte tot el que hi ha en aquest 2n cas
+					;;
+			esac
 		;;
 		2)
 			# Crear Grups
@@ -91,7 +144,7 @@ do
 						((gidFile++))
 						gidDB=$(ldapsearch -x -LLL -b dc=edt,dc=org "(objectClass=posixGroup)" | grep gid | sort -d | cut -d " " -f2 | tail -n 1)
 						((gidDB++))
-						if [ $gidFile > $gidDB]; then
+						if [ $gidFile > $gidDB ]; then
 							gid=$gidFile
 						else
 							gid=$gidDB
