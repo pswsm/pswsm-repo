bc8f2189d02c6b185167bfe348115b4c64d8e1eb
pswsm <pauseawesome@protonmail.com>
2021-02-11 20:12:31 +0100

    He tret la opció de l'editor.

--- sh-script/ldap-menu.sh
+++ sh-script/ldap-menu.sh
@@ -13,9 +13,7 @@ while :
 do
 	clear
 
-	if [[ -f $fAdmin ]]; then
-		continue
-	else
+	if [[ ! -f $fAdmin ]]; then
 		echo -e "\nQuin és l'usuari administrador d'LDAP?"
 		read admin
 		echo -e "\nQuin és el domini? (domain.xyz)"
@@ -34,6 +32,7 @@ do
 			# Carregar a la DB
 			carregar=true
 		while [[ $carregar=true ]]; do
+			clear
 			echo -e "Quins fitxer vols carregar?"
 			echo -e "\n\t1: Usuaris\n\t2: UOs\n\t3: Grups\n\t4: Tot"
 			read tria
@@ -60,6 +59,8 @@ do
 					ldapadd -x -w $contrasenya -D $fAdmin -f $fGroups
 					ldapadd -x -w $contrasenya -D $fAdmin -f $fUsers
 					;;
+				*)
+					echo -e "Germà, entre l'1 i el 4."
 			esac
 			echo -e "Vols fer algo més? [y/N]"
 			read yn
@@ -67,11 +68,9 @@ do
 				y | Y)
 					continue
 					;;
-				n | N)
+				n | N | *)
 					exit 0
 					;;
-				*)
-					exit 0
 			esac
 		done
 		;;
@@ -79,163 +78,131 @@ do
 			# Crear Usuaris
 			clear
 			#DEBUG:
-			#echo -e "\n"$file"\n"
-			echo -e "Vols fer-ho amb manualment (1) o amb l'assistent [2]?"
-			read ed
-			case $ed in
-				1)
-					echo -e "És guradarà a users.ldif\t"
-					$EDITOR $fUsers
-					;;
-				2 | *)
-					echo -e "\nCreador de Grups\n"
-					echo -e "\nUbicació de l'usuari ((uo.)domain.xyz):\t"
-					IFS=. read ou dn1 dn2
-					if [[ -z $dn2 ]]; then
-						#statements
-						dn2=$dn1
-						dn1=$ou
-						unset $cn
-					fi
+			#echo -e "\n"$fUsers"\n"
+			echo -e "És guradarà a "$fUsers
+			echo -e "\nCreador de Grups\n"
+			echo -e "\nUbicació de l'usuari ((uo.)domain.xyz):\t"
+			IFS=. read ou dn1 dn2
+			if [[ -z $dn2 ]]; then
+				#statements
+				dn2=$dn1
+				dn1=$ou
+				unset $cn
+			fi
 
-					# Treure el gidNumber
-					if [ -f $fUsers ]; then
-						gidFile=$(grep uidNumber $fUsers | cut -d " " -f2 | sort -d | tail -n 1)
-						((gidFile++))
-						gidDB=$(ldapsearch -x -LLL -b dc=edt,dc=org "(objectClass=inetOrgPerson)" | grep uidNumber | sort -d | cut -d " " -f2 | tail -n 1)
-						((gidDB++))
-						if [ $gidFile > $gidDB ]; then
-							gid=$gidFile
-						else
-							gid=$gidDB
-						fi
-					else
-						gid=$(ldapsearch -x -LLL -b dc=edt,dc=org "(objectClass=inetOrgPerson)" | grep uidNumber | sort -d | cut -d " " -f2 | tail -n 1)
-					fi
+			# Treure el gidNumber
+			if [ -f $fUsers ]; then
+				gidFile=$(grep uidNumber $fUsers | cut -d " " -f2 | sort -d | tail -n 1)
+				((gidFile++))
+				gidDB=$(ldapsearch -x -LLL -b dc=edt,dc=org "(objectClass=inetOrgPerson)" | grep uidNumber | sort -d | cut -d " " -f2 | tail -n 1)
+				((gidDB++))
+				if [ $gidFile > $gidDB ]; then
+					gid=$gidFile
+				else
+					gid=$gidDB
+				fi
+			else
+				gid=$(ldapsearch -x -LLL -b dc=edt,dc=org "(objectClass=inetOrgPerson)" | grep uidNumber | sort -d | cut -d " " -f2 | tail -n 1)
+				((gid++))
+			fi
 
-					echo -e "El gid serà "$gid"."
-					echo -e "\nNom del usuari:"
-					read nuser
-					if [ -f "$fUsers" ]; then
-						echo -e "\nEl fitxer existeix, vols sobreescriure'l? [Y/n]\t"
-						read yn
-						case $yn in
-							y | Y)
-								echo -e "cn: "$nuser",dn="$dn1",dn="$dn2"\ngidNumber: "$gid"\ncn: "$cn"\nobjectClass: inetOrgPerson\nobjectClass: top\n" > $fUsers
-								;;
-							n | N)
-								echo -e "El contingut serà afegit al final del fitxer.\n"
-								echo -e "cn: "$nuser",dn="$dn1",dn="$dn2"\ngidNumber: "$gid"\ncn: "$cn"\nobjectClass: inetOrgPerson\nobjectClass: top\n" >> $fUsers
-								;;
-								*)
-								echo -e "dn:\t"$cn","$ub"\ngidNumber: "$gid"\ncn: "$cn"\nobjectClass: inetOrgPerson\nobjectClass: top\n" > $fUsers
-						esac
-					else
-						echo -e "dn: "$cn","$ub"\ngidNumber: "$gid"\ncn: "$cn"\nobjectClass: inetOrgPerson\nobjectClass: top\n" > $fUsers
-					fi
-					# $_base/assistent.sh
-					# El fitxer assistent conte tot el que hi ha en aquest 2n cas
-					;;
-			esac
-		;;
+			echo -e "El gid serà "$gid"."
+			echo -e "\nNom del usuari:"
+			read nuser
+			if [[ -f "$fUsers" ]]; then
+				echo -e "\nEl fitxer existeix, vols sobreescriure'l? [Y/n]\t"
+				read yn
+				case $yn in
+					y | Y | *)
+						echo -e "cn: "$nuser",dn="$dn1",dn="$dn2"\ngidNumber: "$gid"\ncn: "$cn"\nobjectClass: inetOrgPerson\nobjectClass: top\n" > $fUsers
+						;;
+					n | N)
+						echo -e "El contingut serà afegit al final del fitxer.\n"
+						echo -e "cn: "$nuser",dn="$dn1",dn="$dn2"\ngidNumber: "$gid"\ncn: "$cn"\nobjectClass: inetOrgPerson\nobjectClass: top\n" >> $fUsers
+						;;
+				esac
+			else
+				echo -e "cn: "$nuser",dn="$dn1",dn="$dn2"\ngidNumber: "$gid"\ncn: "$cn"\nobjectClass: inetOrgPerson\nobjectClass: top\n" > $fUsers
+			fi
+			;;
 		2)
 			# Crear Grups
 			clear
 			#DEBUG:
 			#echo -e "\n"$file"\n"
-			echo -e "Vols fer-ho amb manualment (1) o amb l'assistent [2]?"
-			read ed
-			case $ed in
-				1)
-					echo -e "És guradarà a groups.ldif\t"
-					$EDITOR $fGroups
-					;;
-				2 | *)
-					echo -e "\nCreador de Grups\n"
-					echo -e "\nNom de domini (ou=X,dc=Y,dc=tld):\t"
-					read ub
+			echo -e "\nCreador de Grups\n"
+			echo -e "\nNom de domini (ou=X,dc=Y,dc=tld):\t"
+			read ub
 
-					# Treure el gidNumber
-					if [ -f $fGroups ]; then
-						gidFile=$(grep gid $fGroups | cut -d " " -f2 | sort -d | tail -n 1)
-						((gidFile++))
-						gidDB=$(ldapsearch -x -LLL -b dc=edt,dc=org "(objectClass=posixGroup)" | grep gid | sort -d | cut -d " " -f2 | tail -n 1)
-						((gidDB++))
-						if [ $gidFile > $gidDB ]; then
-							gid=$gidFile
-						else
-							gid=$gidDB
-						fi
-					else
-						gid=$(ldapsearch -x -LLL -b dc=edt,dc=org "(objectClass=posixGroup)" | grep gid | sort -d | cut -d " " -f2 | tail -n 1)
-					fi
+			# Treure el gidNumber
+			if [ -f $fGroups ]; then
+				gidFile=$(grep gid $fGroups | cut -d " " -f2 | sort -d | tail -n 1)
+				((gidFile++))
+				gidDB=$(ldapsearch -x -LLL -b dc=edt,dc=org "(objectClass=posixGroup)" | grep gid | sort -d | cut -d " " -f2 | tail -n 1)
+				((gidDB++))
+				if [ $gidFile > $gidDB ]; then
+					gid=$gidFile
+				else
+					gid=$gidDB
+				fi
+			else
+				gid=$(ldapsearch -x -LLL -b dc=edt,dc=org "(objectClass=posixGroup)" | grep gid | sort -d | cut -d " " -f2 | tail -n 1)
+			fi
 
-					echo -e "El gid serà "$gid"."
-					echo -e "\nNom del grup (cn=Nom):\t"
-					read cn
-					if [ -f "$fGroups" ]; then
-						echo -e "\nEl fitxer existeix, vols sobreescriure'l? [Y/n]\t"
-						read yn
-						case $yn in
-							y | Y)
-								echo -e "dn: "$cn","$ub"\ngidNumber: "$gid"\ncn: "$cn"\nobjectClass: posixGroup\nobjectClass: top\n" > $fGroups
-								;;
-							n | N)
-								echo -e "El contingut serà afegit al final del fitxer.\n"
-								echo -e "dn: "$cn","$ub"\ngidNumber: "$gid"\ncn: "$cn"\nobjectClass: posixGroup\nobjectClass: top\n" >> $fGroups
-								;;
-								*)
-								echo -e "dn: "$cn","$ub"\ngidNumber: "$gid"\ncn: "$cn"\nobjectClass: posixGroup\nobjectClass: top\n" > $fGroups
-						esac
-					else
+			echo -e "El gid serà "$gid"."
+			echo -e "\nNom del grup (cn=Nom):\t"
+			read cn
+			if [ -f "$fGroups" ]; then
+				echo -e "\nEl fitxer existeix, vols sobreescriure'l? [Y/n]\t"
+				read yn
+				case $yn in
+					y | Y)
 						echo -e "dn: "$cn","$ub"\ngidNumber: "$gid"\ncn: "$cn"\nobjectClass: posixGroup\nobjectClass: top\n" > $fGroups
-					fi
-					# $_base/assistent.sh
-					# El fitxer assistent conte tot el que hi ha en aquest 2n cas
-					;;
-			esac
+						;;
+					n | N)
+						echo -e "El contingut serà afegit al final del fitxer.\n"
+						echo -e "dn: "$cn","$ub"\ngidNumber: "$gid"\ncn: "$cn"\nobjectClass: posixGroup\nobjectClass: top\n" >> $fGroups
+						;;
+						*)
+						echo -e "dn: "$cn","$ub"\ngidNumber: "$gid"\ncn: "$cn"\nobjectClass: posixGroup\nobjectClass: top\n" > $fGroups
+				esac
+			else
+				echo -e "dn: "$cn","$ub"\ngidNumber: "$gid"\ncn: "$cn"\nobjectClass: posixGroup\nobjectClass: top\n" > $fGroups
+			fi
+			# $_base/assistent.sh
+			# El fitxer assistent conte tot el que hi ha en aquest 2n cas
 			;;
 		1)
 			#Crear UOs
 			clear
-			echo -e "Vols fer-ho amb manualment (1) o amb l'assistent [2]?"
-			read ed
-			case $ed in
-				1)
-					echo -e "El fitxer serà $fUos."
-					$EDITOR $fUos
-					;;
-				2 | *)
-					echo -e "\nCreador d'UO\n"
-					echo -e "\nNom de domini (dc=X,dc=tld):\t"
-					read dn
-					echo -e $dn >> $dnBase
+			echo -e "\nCreador d'UO\n"
+			echo -e "\nNom de domini (dc=X,dc=tld):\t"
+			read dn
+			echo -e $dn >> $dnBase
 
-					echo -e "\nNom de la UO (ou=UO):\t"
-					read uo
-					echo -e $uo","$dn >> $dnBase
+			echo -e "\nNom de la UO (ou=UO):\t"
+			read uo
+			echo -e $uo","$dn >> $dnBase
 
-					if [ -f "$fUos" ]; then
-						echo -e "\nEl fitxer existeix, vols sobreescriure'l? [Y/n]\t"
-						read yn
-						case $yn in
-							y | Y)
-								echo -e "dn: "$uo","$dn"\nobjectClass: organizationalUnit\nobjectClass: top\nou: "$uo"\n" > $fUos
-								;;
-							n | N)
-								echo -e "\ndn: "$uo","$dn"\nobjectClass: organizationalUnit\nobjectClass: top\nou: "$uo"\n" >> $fUos
-								;;
-							*)
-								echo -e "dn: "$uo","$dn"\nobjectClass: organizationalUnit\nobjectClass: top\nou: "$uo"\n" > $fUos
-								;;
-						esac
-					else
+			if [ -f "$fUos" ]; then
+				echo -e "\nEl fitxer existeix, vols sobreescriure'l? [Y/n]\t"
+				read yn
+				case $yn in
+					y | Y)
 						echo -e "dn: "$uo","$dn"\nobjectClass: organizationalUnit\nobjectClass: top\nou: "$uo"\n" > $fUos
-					fi
-					# $_base/assistent.sh
-					# El fitxer assistent conte tot el que hi ha en aquest 2n cas
-					;;
-			esac
+						;;
+					n | N)
+						echo -e "\ndn: "$uo","$dn"\nobjectClass: organizationalUnit\nobjectClass: top\nou: "$uo"\n" >> $fUos
+						;;
+					*)
+						echo -e "dn: "$uo","$dn"\nobjectClass: organizationalUnit\nobjectClass: top\nou: "$uo"\n" > $fUos
+						;;
+				esac
+			else
+				echo -e "dn: "$uo","$dn"\nobjectClass: organizationalUnit\nobjectClass: top\nou: "$uo"\n" > $fUos
+			fi
+			# $_base/assistent.sh
+			# El fitxer assistent conte tot el que hi ha en aquest 2n cas
 			;;
 		*)
 		clear
