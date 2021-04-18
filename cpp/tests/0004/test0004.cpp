e9ff9fb4b30be2f86bc45d5b1bf7c03ff565cbef
pswsm <pauseawesome@protonmail.com>
2021-04-18 22:05:47 +0200

    Feta tota uf2-pr2, amb alguns petits defectes i falta un exercici (12).

---
+++ cpp/tests/0004/test0004.cpp
@@ -0,0 +1,16 @@
+#include "iostream"
+
+class claseTest {
+private:
+  int algo = 100;
+  std::string altres = "lol, very funny";
+
+public:
+  claseTest (algo, altres);
+  virtual ~claseTest ();
+};
+
+int main() {
+  
+  return 0;
+}
