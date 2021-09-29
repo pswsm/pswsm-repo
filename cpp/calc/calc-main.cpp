e746c4ad99c111482b1623972232b9570754dda1
pswsm <pauseawesome@protonmail.com>
2021-09-29 15:03:06 +0200

    Millorada la calculadora amb un bucle infinit

--- cpp/calc/calc-main.cpp
+++ cpp/calc/calc-main.cpp
@@ -1,39 +1,51 @@
 #include <iostream>
 
+using namespace std;
+
+float suma(float a, float b) {
+  float r; r = a + b; return r;
+}
+
 int main() {
   // Variables
-  float n1, n2, res;
-  char op;
+  float n1, n2, res; char op, qC;
+  system("clear");
 
-  //Lectura i escriptura
-  std::cout << "Numbers (2)\n";
-  std::cin >> n1 >> n2;
-  std::cout << "Operació" << '\n';
-  std::cin >> op;
-
-  //El switch de la calculadora
-  //Determina quina operació es farà segons l'operador (op)
-  switch (op) {
-    case '+':
-      std::cout << n1 << "+" << n2 << '=' << n1 + n2 << '\n';
-      res = n1 + n2;
+  while (true) {
+    cout << "Introdueixi operació:\n";
+    cin >> n1 >> op >> n2;
+    //El switch de la calculadora
+    //Determina quina operació es farà segons l'operador (op)
+    switch (op) {
+      case '+':
+      // res = suma(n1, n2);
+      cout << suma(n1, n2) << endl;
       break;
-    case '-':
-      std::cout << n1 << "-" << n2 << '=' << n1 - n2 << '\n';
-      res = n1 - n2;
+      case '-':
+      cout << n1 << "-" << n2 << '=' << n1 - n2 << '\n';
+      // res = n1 - n2;
       break;
-    case '*':
-      std::cout << n1 << "*" << n2 << '=' << n1 * n2 << '\n';
-      res = n1 * n2;
+      case '*':
+      cout << n1 << "*" << n2 << '=' << n1 * n2 << '\n';
+      // res = n1 * n2;
       break;
-    case '/':
-      std::cout << n1 << "/" << n2 << '=' << n1 / n2 << '\n';
-      res = n1 / n2;
+      case '/':
+      cout << n1 << "/" << n2 << '=' << n1 / n2 << '\n';
+      // res = n1 / n2;
       break;
-    default:
-      std::cout << "Bad Operator: " << op << " not found!\n";
+      default:
+      cout << "Bad Operator: " << op << " not found!\n";
+      // kLoop = false;
       break;
+    }
+    cout << "Vols fer una altra operació?[y/n]" << '\n';
+    cin >> qC;
+    if (qC == 'n' || qC == 'N') {
+      cout << endl;
+      return 0;
+    } else {
+      system("clear");
+      continue;
+    }
   }
-
-  return 0;
 }
