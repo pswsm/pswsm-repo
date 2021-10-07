45dacd6db27b46a9345fc2d62da289f2ad86ab47
Pau Figueras <pauseawesome@pm.me>
2021-10-07 16:18:29 +0200

    Refet els imports d'acord amb els estàndards que dona l\'atom

--- python/python-turtle/turtle001.py
+++ python/python-turtle/turtle001.py
@@ -1,19 +1,23 @@
-from turtle import *
+from turtle import penup, goto, pendown, clear, setup, title, circle, right
+from turtle import forward, left, colormode, xcor, ycor, speed, pencolor
 import os
-from random import *
+from random import randint, choice
 
 leftRight = ["left", "right"]
 dirShape = ["direction", "shape"]
+
 
 def goCenter():
     penup()
     goto(0, 0)
     pendown()
 
+
 def goStart():
     penup()
     goto(-50, -50)
     pendown()
+
 
 def triangle():
     clear()
@@ -24,6 +28,7 @@ def triangle():
         forward(100)
         left(120)
 
+
 def quadrat():
     clear()
     setup(500, 500, 0, 0)
@@ -31,17 +36,20 @@ def quadrat():
     goStart()
     circle(50, None, 4)
 
+
 def pentagon():
     clear()
     setup(500, 500, 0, 0)
     title("Pentagon")
     circle(50, None, 5)
 
+
 def octagon():
     clear()
     setup(500, 500, 0, 0)
     title("Octagon")
     circle(50, 360, 8)
+
 
 def randomLines():
     traces = randint(3, 250)
@@ -52,10 +60,9 @@ def randomLines():
     colormode(255)
     speed(0)
     for i in range(traces):
-        # print(f"Queden {str(traces - i)}")
         if (xcor() > 501 or xcor() < -501) or (ycor() > 501 or ycor() < -501):
             goto(randint(-1249, 1249), randint(-749, 749))
-        pencolor(randint(0, 255),randint(0, 255),randint(0, 255))
+        pencolor(randint(0, 255), randint(0, 255), randint(0, 255))
         chDirShape = choice(dirShape)
         if chDirShape == "direction":
             forward(randint(0, 1000))
@@ -68,24 +75,31 @@ def randomLines():
             circle(randint(5, 500), randint(10, 360), randint(3, 10000))
 
 
-figures = ["Triangle", "Quadrat", "Pentagon", "Octagon", "Random", "Random entre opcions"]
+figures = [
+    "Triangle",
+    "Quadrat",
+    "Pentagon",
+    "Octagon",
+    "Random",
+    "Random entre opcions",
+]
 
 while 1:
     for i, value in enumerate(figures):
         print(f"{i+1}. {figures[i]}")
     figura = input("Quina figura vols fer?\n").lower()
-    os.system('clear')
-    if "triangle" in figura or figura == '1':
+    os.system("clear")
+    if "triangle" in figura or figura == "1":
         triangle()
-    elif "quadrat" in figura or figura == '2':
+    elif "quadrat" in figura or figura == "2":
         quadrat()
-    elif "pentagon" in figura or figura == '3':
+    elif "pentagon" in figura or figura == "3":
         pentagon()
-    elif "octagon" in figura or figura == '4':
+    elif "octagon" in figura or figura == "4":
         octagon()
-    elif "random" in figura or figura == '5':
+    elif "random" in figura or figura == "5":
         randomLines()
-    elif "random enter opcions" in figura or figura == '6':
+    elif "random enter opcions" in figura or figura == "6":
         randomFigura = randint(0, 4)
         if randomFigura == 0:
             triangle()
@@ -98,10 +112,9 @@ while 1:
         elif randomFigura == 4:
             randomLines()
 
-
     qc = input("\nVols continuar? [S/n]")
-    if qc == 'n' or qc == 'N':
+    if qc == "n" or qc == "N":
         exit(0)
     else:
         continue
-    os.system('clear')
+    os.system("clear")
