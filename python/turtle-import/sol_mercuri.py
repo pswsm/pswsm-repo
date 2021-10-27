63467ce50f6f83f0bf6b0b9573b01873c7325842
Pau Figueras <pauseawesome@protonmail.com>
2021-10-27 19:43:40 +0200

    Canviat alguns radius per size al sistema solar

--- python/turtle-import/sol_mercuri.py
+++ python/turtle-import/sol_mercuri.py
@@ -2,22 +2,22 @@
 import turtle
 
 
-def draw_sol(color: str = "#ffbf00", pos: tuple = (0, -50), radius: int = 100):
+def draw_sol(color: str = "#ffbf00", pos: tuple = (0, -50), size: int = 100):
     """Draws the sun"""
     turtle.color(color)
     turtle.penup()
     turtle.goto(pos[0], pos[1])
     turtle.begin_fill()
-    turtle.circle(radius)
+    turtle.circle(size)
     turtle.end_fill()
 
 
 def draw_mercuri(color: str = "#b2beb5", pos: tuple = (100, 150),
-                 radius: int = 10):
+                 size: int = 10):
     """Draws planet mercury"""
     turtle.color(color)
     turtle.penup()
     turtle.goto(pos[0], pos[1])
     turtle.begin_fill()
-    turtle.circle(radius)
+    turtle.circle(size)
     turtle.end_fill()
