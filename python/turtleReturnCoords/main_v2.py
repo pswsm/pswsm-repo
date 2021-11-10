0c1c205f347bc02936b9f73ed1bf05360989d28b
Pau Figueras <pauseawesome@protonmail.com>
2021-11-10 21:37:20 +0100

    Made sqaure a class and made my first python constructor Does the same thing that main.py does, but using a class and catalan named variables: vertices sounded too spanish

---
+++ python/turtleReturnCoords/main_v2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#-- encoding: UTF-8
+
+#made by pswsm
+
+class Square:
+    x: float
+    y: float
+    length: float
+
+    # Constructor
+    def __init__(self, x: float, y: float, length: float):
+        self.x = x
+        self.y = y
+        self.length = length
+      
+    def vertex(self) -> list:
+        x_plus = self.x + self.length/2
+        x_less = self.x - self.length/2
+        y_plus = self.y + self.length/2
+        y_less = self.y - self.length/2
+        vertex_dr = ((x_plus), (y_less))
+        vertex_dl = ((x_less), (y_less))
+        vertex_ur = ((x_plus), (y_plus))
+        vertex_ul = ((x_less), (y_plus))
+        return vertex_dr, vertex_ur, vertex_ul, vertex_dl
+
+
+s: Square = Square(0, 0, 10)
+vertex = s.vertex()
+position = ['down-right', 'upper-right', 'upper-left', 'down-left']
+
+for i in range(len(position)):
+    print(f'Vertex {position[i]}:\t{vertex[i]}')
