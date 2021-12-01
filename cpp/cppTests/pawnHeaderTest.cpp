0bd7b37974a1123c03ff232d79224f82c0af72dc
Pau Figueras <pauseawesome@protonmail.com>
2021-12-01 21:35:17 +0100

    Testing with header files and whatnot

---
+++ cpp/cppTests/pawnHeaderTest.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "pawns.h"
+
+int main() {
+  Pawn p {20, 232, "Pau", "Figueras", "pswsm", true};
+
+  for (std::string n : p.getEverything()) {
+    std::cout << "Data: " << n << endl;
+  }
+}
