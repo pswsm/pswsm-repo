83c9c473e3629d35e6ef313ab7cc35c3c546f897
pswsm <pauseawesome@protonmail.com>
2022-02-05 00:25:57 +0100

    Added new  tables

--- sql/tableTest.sql
+++ sql/tableTest.sql
@@ -1,5 +1,28 @@
 -- DROP TABLES
-drop table zona
-drop table hotel
+drop table habitacio
+drop table hotel;
+drop table zona;
 
-create table zona (id_zona number(3), nom varchar(15), primary key )
+-- CREATE TABLES
+--------------------
+create table zones (
+  id_zona number(3),
+  nom varchar2(15),
+  constraint id_zona_pk primary key (id_zona)
+);
+
+create table hotels (
+  nom_hotel varchar2(20),
+  num_habitacions number(3),
+  id_zona number(3),
+  constraint zones_id_zona_fk foreign key (id_zona) references zones(id_zona) on delete cascade
+);
+
+create table habitacions (
+  nom_hotel varchar2(20),
+  numero number(3),
+  planta number(5),
+  constraint nom_hotel_num_hab_pk primary key (numero, nom_hotel),
+  constraint htls_nom_hotel_fk foreign key (nom_hotel) references hotels(nom_hotel) on delete cascade
+);
+--------------------
