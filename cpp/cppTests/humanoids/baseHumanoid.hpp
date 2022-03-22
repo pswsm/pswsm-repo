57d1d8d624d0dd26abb26d2a32ac9c2c1293ecd7
Pau Figueras <pau@pswsm.cat>
2022-03-22 21:30:22 +0100

    Created blood class. Kinda hard

--- cpp/cppTests/humanoids/baseHumanoid.hpp
+++ cpp/cppTests/humanoids/baseHumanoid.hpp
@@ -3,19 +3,51 @@
 
 #include <string>
 #include <vector>
-#include <ctime>
 
 namespace human {
-  class Human {
-    int natAge, bioAge;
-    std::string firstName, lastName, nickName;
-    bool isAlive;
-    char sex;
+  class Blood {
+    /* Blood Type.
+     * Has a genotype: for future reproduction simulation
+     * Has a fenotype: for displaying the actual bloodtype */ 
+    std::string genotype, fenotype;
+
+    std::string deductBloodFenotype(std::string gen) {
+      if ((gen == "aa") || (gen == "ao")) {
+        return "A";
+      } else if ((gen == "bb") || (gen == "bo")) {
+        return "B";
+      } else if (gen == "oo") {
+        return "O";
+      } else if (gen == "ab") {
+        return "AB";
+      } else {
+        return "??";
+      }
+    }
 
     public:
-    Human(int naturalAge, int biologicalAge, std::string fName, std::string lName, std::string niName, bool alive, char sx): natAge{naturalAge}, bioAge{biologicalAge},
-      firstName{fName}, lastName{lName}, nickName{niName}, isAlive{alive}, sex{sx}
-    {};
+    Blood();
+    Blood(std::string gnt): genotype{gnt}
+    { fenotype = deductBloodFenotype(genotype); };
+
+    std::string getBlood() {
+      return fenotype;
+    }
+  };
+
+  class Human {
+    int natAge, bioAge;
+    std::string firstName, lastName, nickName, blood;
+    bool isAlive;
+    char sex;
+    Blood bloodType {blood};
+
+    public:
+    Human(int naturalAge, int biologicalAge, std::string fName, std::string lName, std::string niName, std::string bt, bool alive, char sx): natAge{naturalAge}, bioAge{biologicalAge},
+      firstName{fName}, lastName{lName}, nickName{niName}, blood{bt}, isAlive{alive}, sex{sx}
+    {
+      // bloodType {blood};
+    };
 
     int getNatAge() {
       return natAge;
@@ -50,23 +82,34 @@ namespace human {
       }
     }
 
+    std::string getBloodType() {
+      return bloodType.getBlood();
+      // return blood;
+    }
+
     std::vector<std::string> getEverything() {
       std::vector<std::string> v = {};
-      v.push_back(std::to_string(natAge));
-      v.push_back(std::to_string(bioAge));
       v.push_back(firstName);
       v.push_back(nickName);
       v.push_back(lastName);
+      v.push_back(std::to_string(natAge));
+      v.push_back(std::to_string(bioAge));
       v.push_back(getSex());
+      v.push_back(getBloodType());
       v.push_back(getAlive());
       return v;
     }
   };
 
+
   std::string selectName(std::vector<std::string> possibleNames) {
-    // srand(time(nullptr));
     std::string selectedName = possibleNames[random() % possibleNames.size()];
     return selectedName;
+  }
+
+  std::string selectBloodGenotype(std::vector<std::string> bts) {
+    std::string selectedBloodGenotype = bts[random() % bts.size()];
+    return selectedBloodGenotype;
   }
 }
 
--- cpp/cppTests/humanoids/humanoidTest.cpp
+++ cpp/cppTests/humanoids/humanoidTest.cpp
@@ -3,24 +3,28 @@
 #include "/home/pswsm/github/pswsm-repo/cpp/cppTests/humanoids/baseHumanoid.hpp"
 
 void printHumanData(human::Human humanToPrint) {
-  std::string humanValues[7] = {"Natural Age", "Biological Age", "First Name", "Last Name", "Nick Name", "Sex", "Currently"};
+  std::string humanValues[8] = {"First Name", "Last Name", "Nick Name", "Natural Age", "Biological Age", "Sex", "Blood", "Currently"};
   for (int j = 0; std::string n : humanToPrint.getEverything()) {
-    std::cout << humanValues[j] << ": " << n << std::endl;
+    std::cout << humanValues[j] << ":\t" << n << std::endl;
     j++;
   };
 }
 
 int main() {
-  std::vector<std::string> possibleNames = {"Denys", "Pau", "Victor", "Gabriel", "Manin", "Basado", "Nose", "Fernando"};
+  std::vector<std::string> possibleNames = {"Denys", "Pau", "Victor", "Gabriel", "Manin", "Basado", "Nose", "Fernando", "Men", "Jordi", "Toni", "JUJA", "Xavi"};
+  std::vector<std::string> possibleBlood = {"aa", "ao", "oo", "ab", "bb", "bo", "cc"};
   std::vector<human::Human> humanPtrs;
-  humanPtrs.reserve(100);
+  humanPtrs.reserve(25);
 
   for (int i = 0; i < 100; i++) {
-    humanPtrs.push_back(*new human::Human {i, i, human::selectName(possibleNames), std::to_string(i), std::to_string(i), 1, 'y'});
+    humanPtrs.push_back(*new human::Human {i, i, human::selectName(possibleNames), std::to_string(i), std::to_string(i), human::selectBloodGenotype(possibleBlood), 1, 'y'});
   };
 
   for (human::Human human : humanPtrs) {
     printHumanData(human);
+    std::cout <<std::endl;
   };
+
+  std::cout << sizeof(humanPtrs);
 }
 
