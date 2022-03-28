047cc73769053fc3161c8efe997acc677ca39341
Pau Figueras <53122461+pswsm@users.noreply.github.com>
2022-03-28 17:18:37 +0200

    Remove compiled rust things

--- rust/testing/target/debug/deps/testing-cba001bc1ae07d00.d
+++
@@ -1,5 +0,0 @@
-/home/pswsm/github/pswsm-repo/rust/testing/target/debug/deps/testing-cba001bc1ae07d00.rmeta: src/main.rs
-
-/home/pswsm/github/pswsm-repo/rust/testing/target/debug/deps/testing-cba001bc1ae07d00.d: src/main.rs
-
-src/main.rs:
--- rust/testing/target/debug/testing.d
+++
@@ -1,1 +0,0 @@
-/home/pswsm/github/pswsm-repo/rust/testing/target/debug/testing: /home/pswsm/github/pswsm-repo/rust/testing/src/main.rs
