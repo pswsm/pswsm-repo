0a34290df0922f20d4f4b96da049b1acbdddcf3e
pswsm <pauseawesome@protonmail.com>
2022-03-28 22:21:46 +0200

    Deleted cargo things

--- rust/humans/target/debug/deps/humans-f1e799b69795b29c.d
+++
@@ -1,5 +0,0 @@
-/home/pswsm/github/pswsm-repo/rust/humans/target/debug/deps/humans-f1e799b69795b29c: src/main.rs
-
-/home/pswsm/github/pswsm-repo/rust/humans/target/debug/deps/humans-f1e799b69795b29c.d: src/main.rs
-
-src/main.rs:
--- rust/humans/target/debug/deps/rand_chacha-9c2bea043a2e7b63.d
+++
@@ -1,9 +0,0 @@
-/home/pswsm/github/pswsm-repo/rust/humans/target/debug/deps/rand_chacha-9c2bea043a2e7b63.rmeta: /home/pswsm/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /home/pswsm/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /home/pswsm/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
-
-/home/pswsm/github/pswsm-repo/rust/humans/target/debug/deps/librand_chacha-9c2bea043a2e7b63.rlib: /home/pswsm/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /home/pswsm/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /home/pswsm/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
-
-/home/pswsm/github/pswsm-repo/rust/humans/target/debug/deps/rand_chacha-9c2bea043a2e7b63.d: /home/pswsm/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /home/pswsm/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /home/pswsm/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
-
-/home/pswsm/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs:
-/home/pswsm/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs:
-/home/pswsm/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs:
