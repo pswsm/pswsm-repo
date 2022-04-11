4daa306ab32ebb0750b926088eeefac08ac18c75
pswsm <pauseawesome@protonmail.com>
2022-04-12 01:30:06 +0200

    Arguments working

--- rust/files_adn_args/src/main.rs
+++ rust/files_adn_args/src/main.rs
@@ -1,59 +1,84 @@
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use std::io::prelude::*;
+use std::io::BufReader;
 use std::fs::File;
 use structopt::StructOpt;
-use std::convert::AsRef;
-use strum_macros::AsRefStr;
 //use std::vec;
 
-#[derive(AsRefStr)]
-enum Options {
-    print,
-    cut,
-    generate
+#[derive(StructOpt)]
+#[structopt(name = "CLI fasta toolkit",
+            about = "A CLI toolkit to generate, edit and see fasta files",
+            rename_all = "kebab-case")]
+struct Args {
+    #[structopt(subcommand)]
+    cmdline: Command
 }
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "argument handling", about = "Reads a file, does something with its contents, writes to another file. All this theoretically.", rename_all = "kebab-case")]
-struct Args {
-    #[structopt(short, long = "input", help = "File to read", parse(from_os_str))]
+enum Command {
+    Print(CatOptions),
+    Cut(CutOptions),
+    Generate(GenerateOptions)
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "print file options",
+            about = "Reads file, prints its content",
+            rename_all = "kebab-case")]
+struct CatOptions {
+    #[structopt(short, long = "input", help = "The file to read")]
     input_file_name: PathBuf,
-    
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "cutting options",
+            about = "Cuts nucleotides from..to range",
+            rename_all = "kebab-case")]
+struct CutOptions {
     #[structopt(short, long = "output", help = "File to write", parse(from_os_str))]
     output_file_name: PathBuf,
+    
+    #[structopt(short, long = "input", help = "File to read", parse(from_os_str))]
+    input_file_name: PathBuf,
 
-    main_op: String,
+    #[structopt(short, long, help = "Range(s) to cut")]
+    range: String
+}
 
+#[derive(Debug, StructOpt)]
+#[structopt(name = "generation options",
+            about = "Generates a fasta file long n nucleotides",
+            rename_all = "kebab-case")]
+struct GenerateOptions {
+    #[structopt(short = "n", long, help = "Number of bases")] 
+    length: u32,
 }
 
 fn main() {
     let args = Args::from_args();
 
-    let print: Options = Options::print;
-    let cut: Options = Options::cut;
-    let generate: Options = Options::generate;
-
-    let print_as_ref = print.as_ref();
-    let cut_as_ref = cut.as_ref();
-    let generate = generate.as_ref();
-
-    match args.main_op {
-        print_as_ref => print_file(args.input_file_name),
-        //cut_as_ref => cut(),
-        //generate_as_ref => generate(,)
-        _ => println!("{} is not an option. See [-h --help] for more info.", args.main_op)
-    }
-
+    match args.cmdline {
+        Command::Cut(args) => cutting(args.input_file_name, args.output_file_name, args.range).unwrap(),
+        Command::Generate(args) => generate(args.length).unwrap(),
+        Command::Print(args) => cat(args.input_file_name).unwrap(),
+    };
 }
 
-fn print_file(file: PathBuf) {
+fn cutting(input_file_path: PathBuf, output_file_path: PathBuf, range: String) -> std::io::Result<()> {
+    println!("Reading {}\nWriting {} with range {}", input_file_path.into_os_string().into_string().unwrap(), output_file_path.into_os_string().into_string().unwrap(), range);
+    Ok(())
+}
 
-    let mut open_file = File::open(file).expect("Can't open desired file.");
-    let mut file_data: String = String::new();
+fn cat(input_file: PathBuf) -> std::io::Result<()> {
+    let file = File::open(input_file)?;
+    let mut reader = BufReader::new(file);
+    let mut contents: String = String::new();
+    reader.read_to_string(&mut contents)?;
+    println!("{}", contents);
+    Ok(())
+}
 
-    open_file.read_to_string(&mut file_data).expect("Can't read file.");
-
-    println!("{}", file_data);
-
-
+fn generate(length: u32) -> std::io::Result<()> {
+    println!("Length to generate is: {}", length);
+    Ok(())
 }
