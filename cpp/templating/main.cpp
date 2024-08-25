ff7323489587fddb6fbcee18f880f30973eedada
pswsm <53122461+pswsm@users.noreply.github.com>
2024-08-25 03:48:17 +0200

    feat: templating engine first commit
    and last

---
+++ cpp/templating/main.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+class Token {
+  private:
+    char m_l_separator {};
+    std::string m_key {};
+    char m_r_separator {};
+
+  public:
+    Token(std::string toToken) {
+      toToken.:q
+    };
+};
+
+class HtmlTemplate {
+  private:
+    std::pmr::vector<Token> tokens
+}
