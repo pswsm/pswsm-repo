289d98dfd93c2f6001086bf7b39286cd359158a0
pswsm <53122461+pswsm@users.noreply.github.com>
2024-09-25 17:52:54 +0200

    build: update uesrs sql

--- gleam/sequences/sql/create_users.sql
+++ gleam/sequences/sql/create_users.sql
@@ -1,5 +1,6 @@
 create table if not exists users (
     id blob primary key,
     username text not null,
-    created_at integer not null
+    password text not null,
+    created_at integer default (strftime('%s', 'now'))
 );
